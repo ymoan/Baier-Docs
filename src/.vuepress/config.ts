ff9904bcf4628990de20defb4286d28eee269db7
云墨安 <ymoan@qq.com>
2025-11-11 18:08:16 +0800

    修改

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/Baier-Docs/",
+  base: "/docs.yunz.cc/",
 
   lang: "zh-CN",
   title: "文档演示",
