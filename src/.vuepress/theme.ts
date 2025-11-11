60ec3c64cf4cf4791267ad907a6a79e1a8894fe1
云墨安 <ymoan@qq.com>
2025-11-11 19:32:25 +0800

    Update theme configuration and metadata

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -1,43 +1,52 @@
 import { hopeTheme } from "vuepress-theme-hope";
+
 import navbar from "./navbar.js";
 import sidebar from "./sidebar.js";
 
 export default hopeTheme({
-  hostname: "https://vuepress-theme-hope-docs-demo.netlify.app",
+  hostname: "https://Yunzai-Bot.com",
 
-  logo: "https://theme-hope-assets.vuejs.press/logo.svg", // 若想换自己的logo，可改为 "/你的logo.png"（放在.vuepress/public文件夹）
-  repo: "ymoan/Baier-Docs", 
-  docsDir: "src",
-
-  // 导航栏
-  navbar,
-
-  // 侧边栏
-  sidebar,
-
-  // 页脚
-  footer: "默认页脚",
-  displayFooter: false,
-
-  // 加密配置
-  encrypt: {
-    config: {
-      "/demo/encrypt.html": {
-        hint: "Password: 1234",
-        password: "1234",
-      },
-    },
+  author: {
+    name: "云墨安",
+    url: "https://ymoan.net/",
   },
 
-  // 多语言配置
+  logo: "/logo.png",
+
+  fullscreen: true,
+
+  repo: "DenFengLai/Yunzai-Docs",
+
+  docsDir: "src",
+  
+  breadcrumb: true,
+
+  navbar,
+
+  sidebar,
+
+  copyright: "Copyright © 2025 小白厄QBOT文档",
+
+  // footer: "页脚",
+
+  displayFooter: true,
+
+  // 加密配置
+  // encrypt: {
+  //   config: {
+  //     "/demo/encrypt.html": {
+  //       hint: "Password: 1234",
+  //       password: "1234",
+  //     },
+  //   },
+  // },
+
   metaLocales: {
     editLink: "在 GitHub 上编辑此页",
   },
 
-  // 如果想要实时查看任何改变，启用它。注: 这对更新性能有很大负面影响
-  // hotReload: true,
+  hotReload: true,
 
-  // 此处开启了很多功能用于演示，你应仅保留用到的功能。
   markdown: {
     align: true,
     attrs: true,
@@ -69,111 +78,23 @@ export default hopeTheme({
     sup: true,
     tabs: true,
     tasklist: true,
-    vPre: true,
-
-    // 取消注释它们如果你需要 TeX 支持
-    // math: {
-    //   // 启用前安装 katex
-    //   type: "katex",
-    //   // 或者安装 @mathjax/src
-    //   type: "mathjax",
-    // },
-
-    // 如果你需要幻灯片，安装 @vuepress/plugin-revealjs 并取消下方注释
-    // revealjs: {
-    //   plugins: ["highlight", "math", "search", "notes", "zoom"],
-    // },
-
-    // 在启用之前安装 chart.js
-    // chartjs: true,
-
-    // insert component easily
-
-    // 在启用之前安装 echarts
-    // echarts: true,
-
-    // 在启用之前安装 flowchart.ts
-    // flowchart: true,
-
-    // 在启用之前安装 mermaid
-    // mermaid: true,
-
-    // playground: {
-    //   presets: ["ts", "vue"],
-    // },
-
-    // 在启用之前安装 @vue/repl
-    // vuePlayground: true,
-
-    // 在启用之前安装 sandpack-vue3
-    // sandpack: true,
+    vPre: true
   },
 
-  // 在这里配置主题提供的插件（已移除 comment 配置 → 关闭评论功能）
+  // 插件配置
   plugins: {
+    
+    // comment: {
+    //   provider: "Waline",
+    //   serverURL: "https://waline.dengfenglai.icu/",
+    // },
+
     components: {
-      components: ["Badge", "VPCard"],
+      components: ["Badge", "VPCard", "VPBanner"],
     },
 
     icon: {
       prefix: "fa6-solid:",
-    },
-
-    // 如果你需要 PWA。安装 @vuepress/plugin-pwa 并取消下方注释
-    // pwa: {
-    //   favicon: "/favicon.ico",
-    //   cacheHTML: true,
-    //   cacheImage: true,
-    //   appendBase: true,
-    //   apple: {
-    //     icon: "/assets/icon/apple-icon-152.png",
-    //     statusBarColor: "black",
-    //   },
-    //   msTile: {
-    //     image: "/assets/icon/ms-icon-144.png",
-    //     color: "#ffffff",
-    //   },
-    //   manifest: {
-    //     icons: [
-    //       {
-    //         src: "/assets/icon/chrome-mask-512.png",
-    //         sizes: "512x512",
-    //         purpose: "maskable",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "/assets/icon/chrome-mask-192.png",
-    //         sizes: "192x192",
-    //         purpose: "maskable",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "/assets/icon/chrome-512.png",
-    //         sizes: "512x512",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "/assets/icon/chrome-192.png",
-    //         sizes: "192x192",
-    //         type: "image/png",
-    //       },
-    //     ],
-    //     shortcuts: [
-    //       {
-    //         name: "Demo",
-    //         short_name: "Demo",
-    //         url: "/demo/",
-    //         icons: [
-    //           {
-    //             src: "/assets/icon/guide-maskable.png",
-    //             sizes: "192x192",
-    //             purpose: "maskable",
-    //             type: "image/png",
-    //           },
-    //         ],
-    //       },
-    //     ],
-    //   },
-    // },
+    }
   },
 });
